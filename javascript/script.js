f0a2ad9ee8730e3aec8355681ad2b605ae292da5
Tobias Stani <stanislavskytobias@gmail.com>
2022-05-14 12:22:03 -0300

    100% fumcional

--- javascript/script.js
+++ javascript/script.js
@@ -1,88 +1,3 @@
-// (function () {
-//   let actualizarHora = function () {
-//     let fecha = new Date(),
-//       horas = fecha.getHours(),
-//       ampm,
-//       minutos = fecha.getMinutes(),
-//       segundos = fecha.getSeconds(),
-//       diaSemana = fecha.getDay(),
-//       dia = fecha.getDate(),
-//       mes = fecha.getMonth(),
-//       year = fecha.getFullYear();
-
-//     let pHoras = document.getElementById("horas");
-//     let pAMPM = document.getElementById("ampm");
-//     let pMinutos = document.getElementById("minutos");
-//     let pSegundos = document.getElementById("segundos");
-//     let pDiaSemana = document.getElementById("diaSemana");
-//     let pDia = document.getElementById("dia");
-//     let pMes = document.getElementById("mes");
-//     let pYear = document.getElementById("year");
-
-//     let semana = [
-//       "Domingo",
-//       "Lunes",
-//       "Martes",
-//       "Miercoles",
-//       "Jueves",
-//       "Viernes",
-//       "Sabado",
-//     ];
-//     pDiaSemana.textContent = semana[diaSemana];
-
-//     pDia.textContent = dia;
-
-//     let meses = [
-//       "Enero",
-//       "Febrero",
-//       "Marzo",
-//       "Abril",
-//       "Mayo",
-//       "Junio",
-//       "Julio",
-//       "Agosto",
-//       "Septiembre",
-//       "Octubre",
-//       "Nobiembre",
-//       "Diciembre",
-//     ];
-//     pMes.textContent = meses[mes];
-
-//     pYear.textContent = year;
-
-//     // reloj
-
-//     if (horas >= 12) {
-//       horas = horas - 12;
-//       ampm = "PM";
-//     } else {
-//       ampm = "AM";
-//     }
-
-//     if (horas == 0) {
-//       horas = 12;
-//     }
-
-//     if (minutos < 10) {
-//       minutos = "0" + minutos;
-//     }
-//     if (segundos < 10) {
-//       segundos = "0" + segundos;
-//     }
-
-//     pHoras.textContent = horas;
-//     pAMPM.textContent = ampm;
-
-//     pMinutos.textContent = minutos;
-//     pSegundos.textContent = segundos;
-
-//     // reloj
-//   };
-
-//   actualizarHora();
-//   let intervalo = setInterval(actualizarHora, 1000);
-// })();
-
 // Lista de tareas
 
 const lista = document.getElementById("lista");
@@ -188,6 +103,7 @@ lista.addEventListener("click", function (event) {
 let data = localStorage.getItem("TODO");
 if (data) {
   LIST = JSON.parse(data);
+  console.log(LIST);
   id = LIST.length;
   cargarLista(LIST);
 } else {
@@ -195,8 +111,8 @@ if (data) {
   id = 0;
 }
 
-function cargarLista(DATA) {
-  DATA.forEach((i) => {
-    agregarTarea(i.nombre, i.id, i.realizado, i.eliminado);
+function cargarLista(array) {
+  array.forEach(function (item) {
+    agregarTarea(item.nombre, item.id, item.realizado, item.eliminado);
   });
 }
